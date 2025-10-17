74e957c17f415cfd864f1be3228af04300dc1313
aabdalaa <andreluizabdalacosta@gmail.com>
2025-10-17 09:42:24 -0300

    Add initial HTML, CSS, and image assets for JS Game project
    - Created index.html with basic structure and game content
    - Added style.css for styling the game interface
    - Included trophy.png image asset for visual feedback

--- logica-js-projeto_inicial/AULA PRINCIPAL/app.js
+++ logica-js-projeto_inicial/AULA PRINCIPAL/app.js
@@ -1,10 +1,39 @@
-alert("Boas vindas ao meu jogo do número secreto em parceria com a Alura!");
-let numsec = 29;
-let player = prompt("Escolha um número entre 1 e 100");
+// A única coisa que precisamos da página é o 'body'
+const body = document.body;
 
-if (numsec == player) {
-    console.log("Parabéns, você acertou o número secreto!")
+alert("Boas vindas ao meu jogo do número secreto em parceria com a Alura!");
+
+const maxTentativas = 5;
+let numsec = 0
+
+while (numsec < 1 || numsec > 100){
+    numsec = prompt("Escolha um número de 1 a 100, mas não deixe o outro jogador ver:")
 }
-else{
-    console.log("Número incorreto, tente novamente!")
+
+
+let player;
+let tentativas = 0;
+let acertou = false;
+
+while (tentativas < maxTentativas) {
+    tentativas++;
+    player = prompt(`Escolha um número entre 1 e 100 (Tentativa ${tentativas} de ${maxTentativas}):`);
+
+    if (numsec == player) {
+        acertou = true;
+        break;
+    } else {
+        if (player > numsec) {
+            alert(`O número secreto é MENOR que ${player}, tente novamente!`);
+        } else {
+            alert(`O número secreto é MAIOR que ${player}, tente novamente!`);
+        }
+    }
+}
+
+// Agora, a única tarefa é trocar a imagem de fundo
+if (acertou) {
+    body.style.backgroundImage = "url('./img/ganhou.png')";
+} else {
+    body.style.backgroundImage = "url('./img/perdeu.png')";
 }
--- logica-js-projeto_inicial/EXERCICIOS/AULA 1/app.js
+++ logica-js-projeto_inicial/EXERCICIOS/AULA 1/app.js
@@ -1,1 +1,20 @@
 alert("Bem-vindo ao Site da Alura!")
+var nome = "lua"
+var idade = 25
+var numeroDeVendas = 50
+var saldoDisponivel = 1000
+
+alert("Preencha todos os campos a seguir!")
+
+var mensagemDeErro = "Erro! Preencha todos os campos!"
+
+var nome = prompt("Qual seu nome?")
+var idade = prompt("Qual sua idade?")
+
+if (idade >= 18){
+    alert("Pode tirar a habilitação, " + nome)
+}
+
+else{
+    alert("Não pode tirar a habilitação, " + nome)
+}
